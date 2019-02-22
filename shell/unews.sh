d0d4d8def17c6244486b5ac602bc7937e9629d8b
Adrian Manea <adrianmanea@fastmail.fm>
2019-02-22 06:23:39 +0200

    add full paths

--- shell/unews.sh
+++ shell/unews.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 SITEDIR='/var/www/adrianmanea.xyz/html'
+MYPATH='/usr/local/bin'
 
 # get new news
-sfeed_update > /dev/null
+$MYPATH/sfeed_update > /dev/null
 # make them into HTML
-sfeed_html $HOME/.sfeed/feeds/* > $HOME/.sfeed/feeds.html
+$MYPATH/sfeed_html $HOME/.sfeed/feeds/* > $HOME/.sfeed/feeds.html
 # put the HTML in the website directory
 cp -f $HOME/.sfeed/feeds.html $SITEDIR/pages/
 echo "NEWS UPDATED\n"
