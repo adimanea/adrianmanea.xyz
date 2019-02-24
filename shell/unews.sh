aff3167f2c9f5e1d5a84f64e1cab2a4d06fc2fe3
Adrian Manea <adrianmanea@fastmail.fm>
2019-02-24 07:36:57 +0200

    rm feeds at each update

--- shell/unews.sh
+++ shell/unews.sh
@@ -3,8 +3,10 @@
 SITEDIR='/var/www/adrianmanea.xyz/html'
 MYPATH='/usr/local/bin'
 
+# remove all feeds (start fresh) -- in case of update
+rm -rf $HOME/.sfeed/feeds/*
 # get new news
-$MYPATH/sfeed_update > /dev/null
+$MYPATH/sfeed_update
 # make them into HTML
 $MYPATH/sfeed_html $HOME/.sfeed/feeds/* > $HOME/.sfeed/feeds.html
 # put the HTML in the website directory
