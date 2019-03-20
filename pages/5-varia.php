596d1d7493b5121c08a6aa68f4ca2d40aea966a4
Adrian Manea <adrianmanea@fastmail.fm>
2019-03-20 13:06:20 +0200

    add last.fm && dotfiles

--- pages/5-varia.php
+++ pages/5-varia.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
 <head>
-    <?php
-    include ("../includes/head.php");
-    ?>
+<?php
+include ("../includes/head.php");
+?>
 </head>
 <header>
-    <?php
-    include ("../includes/header.php");
-    ?>
+<?php
+include ("../includes/header.php");
+?>
 </header>
 <body>
-    <?php
-    include ("../includes/navigation.php");
-    ?>
-    
+<?php
+include ("../includes/navigation.php");
+?>
+
     <h1 class="title">Varia</h1>
     <div class="articol">
-        <p>
-            Sînt multe lucruri care mă entuziasmează, care mă fac curios, care mă pasionează și mă captivează definitiv, fie că este vorba despre domenii de preocupare intelectuală, filosofică, spirituală sau doar de divertisment, amuzament ori pur și simplu care aduc <i>joie de vivre</i>, prin detalii sau caracteristici greu de cuantificat.
-        </p>
+	<p>
+	    Sînt multe lucruri care mă entuziasmează, care mă fac curios, care mă pasionează și mă captivează definitiv, fie că este vorba despre domenii de preocupare intelectuală, filosofică, spirituală sau doar de divertisment, amuzament ori pur și simplu care aduc <i>joie de vivre</i>, prin detalii sau caracteristici greu de cuantificat.
+	</p>
 
-        <p>
-            Sper să pot studia, citi, învăța și scrie cît mai multe despre asemenea lucruri, dar pînă atunci, listez cîteva, care se încadrează, în general, în interesele mele non-academice.
-            <ul>
-                <li><b>Literatura</b> este o componentă foarte importantă a dezvoltării mele și, în general, încerc să dau prioritate clasicilor. O parte a lecturilor mele o puteți găsi urmărindu-mă pe <a href="https://www.goodreads.com/user/show/25653821-adrian-manea" target="_blank">GoodReads</a>.</li>
-                <li>Îmi place să citesc și să iau parte la <b>discuții</b> diverse, argumentative, informative sau doar de divertisment. De aceea, mă găsiți și pe <a href="https://www.reddit.com/user/t3rtius" target="_blank">reddit</a>.</li>
-                <li>Sînt foarte pasionat de <b>stilouri, cerneală și scris de mînă</b>. Nu atît de a le colecționa, ci de a le folosi, pentru notițe, texte, ciorne. Cred că prin a scrie de mînă păstrăm o legătură de o importanță fundamentală cu latura noastră umană, realizăm o conexiune cu simbolurile și cuvintele pe care le naștem din degete, pe hîrtie, cum nu mai este alta. De aceea, chiar dacă, de cele mai multe ori, îmi (și) digitalizez notițele, încep prin a le scrie cu stiloul pe hîrtie.</li>
-                <li>Cred că toată lumea ar trebui să aibă un acces cît mai facil la lectură, documentare și studiu. De aceea, susțin proiecte precum <a href="https://opensciencemooc.eu/" target="_blank"><b>Open Science</b></a> și unelte <b>open source</b> (folosesc zilnic <a href="https://www.gnu.org/software/emacs/" target="_blank">Emacs</a>, <a href="https://www.latex-project.org/" target="_blank">LaTeX</a>, precum și software educațional ca <a href="https://www.sagemath.org/" target="_blank">SageMath</a>).</li>
-                <li>Sînt un pic de <a href="6-foto.php" target="_parent"><b>fotograf amator</b></a>. Deși uneori exagerez cu postprocesarea, mă atrag, în special, fotografiile alb-negru și simple.</li>
-                <li>Nu în ultimul rînd, sursa pentru acest site și documentele din el se pot găsi pe <a href="https://gitlab.com/adimanea/adrianmanea.xyz" target="_blank">GitLab</a>, unde încurajez comentariile și contribuțiile din partea oricui.</li>
-            </ul>
-        </p>
+	<p>
+	    Sper să pot studia, citi, învăța și scrie cît mai multe despre asemenea lucruri, dar pînă atunci, listez cîteva, care se încadrează, în general, în interesele mele non-academice.
+	    <ul>
+		<li><b>Literatura</b> este o componentă foarte importantă a dezvoltării mele și, în general, încerc să dau prioritate clasicilor. O parte a lecturilor mele o puteți găsi urmărindu-mă pe <a href="https://www.goodreads.com/user/show/25653821-adrian-manea" target="_blank">GoodReads</a>.</li>
+		<li>Îmi place să citesc și să iau parte la <b>discuții</b> diverse, argumentative, informative sau doar de divertisment. De aceea, mă găsiți și pe <a href="https://www.reddit.com/user/t3rtius" target="_blank">reddit</a>.</li>
+		<li>Sînt foarte pasionat de <b>stilouri, cerneală și scris de mînă</b>. Nu atît de a le colecționa, ci de a le folosi, pentru notițe, texte, ciorne. Cred că prin a scrie de mînă păstrăm o legătură de o importanță fundamentală cu latura noastră umană, realizăm o conexiune cu simbolurile și cuvintele pe care le naștem din degete, pe hîrtie, cum nu mai este alta. De aceea, chiar dacă, de cele mai multe ori, îmi (și) digitalizez notițele, încep prin a le scrie cu stiloul pe hîrtie. Profilul meu pe FPN este <a href="http://www.fountainpennetwork.com/forum/user/140477-adim" target=_blank><b>aici</a>.</li>
+<li>Am gusturi foarte variate în muzică. O bună parte din melodiile ascultate de mine se găsesc pe <a href="https://www.last.fm/user/t3rt1us" target=_blank><b>last.fm</b></a>.</li>
+		<li>Cred că toată lumea ar trebui să aibă un acces cît mai facil la lectură, documentare și studiu. De aceea, susțin proiecte precum <a href="https://opensciencemooc.eu/" target="_blank"><b>Open Science</b></a> și unelte <b>open source</b>. Fișierele de configurare pentru programele folosite se găsesc <a href="https://gitlab.com/adimanea/my-dotfiles" target=_blank><b>aici</b></a>.</li>
+		<li>Sînt un pic de <a href="6-foto.php" target="_parent"><b>fotograf amator</b></a>. Deși uneori exagerez cu postprocesarea, mă atrag, în special, fotografiile alb-negru și simple.</li>
+		<li>Nu în ultimul rînd, sursa pentru acest site și documentele din el se pot găsi pe <a href="https://gitlab.com/adimanea/adrianmanea.xyz" target="_blank">GitLab</a>, unde încurajez comentariile și contribuțiile din partea oricui.</li>
+	    </ul>
+	</p>
     </div>
 </body>
 </html>
