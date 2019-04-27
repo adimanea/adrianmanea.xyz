85a5e68fc840a6e49f252e9bbb9400a5bc0d8cce
Adrian Manea <adrianmanea@fastmail.fm>
2019-04-27 07:35:12 +0300

    schimbat srand && alinierea

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <!doctype html>
 <meta charset="utf-8">
-<head>
-    <?php
-    include ("includes/head.php");
-    ?>
+             <head>
+<?php
+                  include ("includes/head.php");
+?>
 </head>
 <header>
-    <?php
-    include ("includes/header.php");
-    ?>
+<?php
+include ("includes/header.php");
+?>
 </header>
 <body>
-    <?php
-    include ("includes/navigation.php");
-    ?>
-    <?php
-    $sDirectory     = 'pages-random';
-    if( is_dir( $sDirectory ) ) 
+<?php
+include ("includes/navigation.php");
+?>
+<?php
+$sDirectory     = 'pages-random';
+if( is_dir( $sDirectory ) ) 
+{
+    $rDir = opendir( $sDirectory );
+    while( ( $sFile = readdir( $rDir ) ) !== FALSE ) 
     {
-        $rDir = opendir( $sDirectory );
-        while( ( $sFile = readdir( $rDir ) ) !== FALSE ) 
+        if( ( $sFile == '.' ) || ( $sFile === '..' ) )
         {
-            if( ( $sFile == '.' ) || ( $sFile === '..' ) )
-            {
-                continue;
-            }
-            $aFiles[] = $sDirectory . '/' . $sFile;
+            continue;
         }
+        $aFiles[] = $sDirectory . '/' . $sFile;
     }
-    srand(mktime(0, 0, 0));
-    $sRandom = array_rand( $aFiles );
-    require_once( $aFiles[ $sRandom ] );
-    ?>
+}
+srand(date('z') + 1);
+// srand(mktime(0, 0, 0));
+$sRandom = array_rand( $aFiles );
+require_once( $aFiles[ $sRandom ] );
+?>
 </body>
 </html>
